5b2824b523a1e6bf4eb4b4a4f3341cc76781a0a4
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-10-06 08:47:47 -0500

    Download files from AWS with profile

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,22 +1,37 @@
+from os import environ
 from invoke import task
 import boto3
 import acousticsim
-from path import Path
+from unipath import Path
 
-@task
-def download(ctx, filename=None):
-    """Download the imitations."""
-    ALL_FILES = ['words-in-transition.zip', 'messages']
+@task(help=dict(filename=("The name of the file to download. Defaults to "
+                          "downloading all relevant files."),
+                profile="The name of the AWS profile to use. Optional."))
+def download(ctx, filename=None, profile=None):
+    """Download data from the internet."""
+    BUCKET_NAME = 'words-in-transition'
+
+    ALL_FILES = 'words-in-transition.zip'.split()
     files = [filename] if filename else ALL_FILES
+
+    if profile:
+        environ['AWS_PROFILE'] = profile
+
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('words-in-transition')
-    bucket.download_file(
-        'words-in-transition/words-in-transition.zip',
-        'words-in-transition.zip'
-    )
-    ctx.run('unzip words-in-transition.zip')
-    ctx.run('mkdir data/ && mv webapps/telephone/media/words-in-transition data/imitations')
-    ctx.run('rm -r webapps words-in-transition.zip')
+    for filename in files:
+        bucket.download_file(Path(BUCKET_NAME, filename), filename)
+
+        if filename == 'words-in-transition.zip':
+            # Unpack and cleanup zip
+            cmds = [
+                'unzip words-in-transition.zip',
+                'mkdir data/',
+                'mv webapps/telephone/media/words-in-transition data/imitations',
+                'rm -r webapps words-in-transition.zip'
+            ]
+            for cmd in cmds:
+                ctx.run(cmd)
 
 @task
 def compare(ctx, edges_csv=None):
@@ -25,6 +40,8 @@ def compare(ctx, edges_csv=None):
 
 def acoustic_similarity_chain(sounds, **kwargs):
     mapping = create_mapping_from_chain(sounds)
+    return acousticsim.main.acoustic_similarity_mapping(mapping)
+
 
 def create_mapping_from_chain(sounds):
     for i in range(len(sounds) - 1):
