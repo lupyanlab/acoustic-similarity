dc342f653e83288c7da31df5cd7ed7609874c4d7
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-10-14 09:05:53 -0500

    Basic setup for similarity judgments

---
+++ judgments.py
@@ -0,0 +1,68 @@
+from psychopy import visual, core, event, sound, logging
+
+class SimilarityJudgments(object):
+    """Judge the similarity between two sounds.
+
+    On each trial, two buttons are presented on the screen.
+    Clicking on these buttons playes one of the sounds.
+    A response scale is presented along the bottom of
+    the screen. Players rate the two sounds on a 7-point
+    scale.
+
+    Pressing 'q' during a trial causes the experiment to quit.
+    """
+    KEYBOARD = dict(q='quit')
+    KEYBOARD.update({str(i): i for i in range(1, 8)})
+
+    def __init__(self, player):
+        self.player = player
+        self.win = visual.Window()
+        self.edges = get_edges_to_judge()
+        self.mouse = event.Mouse(win=self.win)
+
+    def run(self):
+        for (sound_x, sound_y) in self.edges:
+            try:
+                self.judge_similarity(sound_x, sound_y)
+            except QuitExperiment:
+                break
+
+    def judge_similarity(sound_x, sound_y):
+        event.clearEvents()
+        self.mouse.clickReset()
+
+        while True:
+            key_responses = event.getKeys(keyList=self.KEYBOARD.keys())
+            mouse_responses = self.mouse.getPressed()
+
+            if key_responses:
+                key = self.KEYBOARD.get(key_response[0])
+                if self.KEYBOARD.get(key) == 'quit':
+                    logging.info('key press requested experiment quit')
+                    raise QuitExperiment
+                print key  # record response
+                break
+
+            if mouse_responses:
+                pos = self.mouse.getPos()
+                if self.sound_x_button.contains(pos):
+                    sound_x.play()
+                elif self.sound_y_button.contains(pos):
+                    sound_y.play()
+                else:
+                    logging.info('mouse press outside of buttons')
+
+
+def get_player_info():
+    return dict(name='pierce')
+
+
+def get_edges_to_judge():
+    return [('1.wav', '2.wav'), ]
+
+
+if __name__ == '__main__':
+    logging.console.setLevel(logging.INFO)
+    player = get_player_info()
+    judgments = SimilarityJudgments(player)
+    judgments.run()
